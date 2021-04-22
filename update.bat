f94514c0903e5e1ce8569b93d7835527d1ac8e3d
kevin010717 <k511153362@gmail.com>
2021-04-22 08:03:08 +0800

    gitbook

--- update.bat
+++ update.bat
@@ -1,9 +1,13 @@
+
+@echo off
 call gitbook epub ./ ./mybook.epub
 call gitbook pdf ./ ./mybook.pdf
 call gitbook mobi ./ ./mybook.mobi
 call gitbook build
-#xcopy _book\*.* ..\_book /s
-xcopy _book\*.* .\ /s
+xcopy _book\*.* .\ /f /s /y
+del _book\*.* /f /s /y
+rd _book /s /Q
+@echo on
 #call git add .
 #call git commit -m "gitbook"
 #call git push origin master
