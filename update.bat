deb38bbbba065a990e374af515393d9e52e21be1
kevin010717 <k511153362@gmail.com>
2021-10-21 19:19:10 +0800

    gitbook

--- update.bat
+++ update.bat
@@ -1,8 +1,11 @@
 @echo off
 call gitbook init
-# call gitbook epub ./ ./wangyishengdegushi.epub
-# call gitbook pdf ./ ./王医生的故事.pdf
-# call gitbook mobi ./ ./王医生的故事.mobi
+#call gitbook epub ./ ./wangyishengdegushi.epub
+#call gitbook pdf ./ ./王医生的故事.pdf
+#call gitbook mobi ./ ./王医生的故事.mobi
+call gitbook epub 
+call gitbook pdf 
+call gitbook mobi
 call gitbook build
 # xcopy _book\*.* .\ /f /s /y
 # del _book\*.* /f /s /y
