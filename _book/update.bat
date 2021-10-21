f08d3889d17312117b28df891d735c9e7b25be23
kevin010717 <k511153362@gmail.com>
2021-10-22 07:58:38 +0800

    gitbook

--- _book/update.bat
+++ _book/update.bat
@@ -7,9 +7,9 @@ call gitbook epub
 call gitbook pdf 
 call gitbook mobi
 call gitbook build
+del .\book.*
 # xcopy _book\*.* .\ /f /s /y
 # del _book\*.* /f /s /y
-del .\book.*
 # rd _book /s /Q
 @echo on
 call git add .
