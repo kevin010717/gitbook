a79dfe902224b3a8545568c08703fefb94d85d58
kevin010717 <k511153362@gmail.com>
2021-10-21 19:17:38 +0800

    gitbook

---
+++ _book/_book/update.bat
@@ -0,0 +1,15 @@
+@echo off
+call gitbook init
+# call gitbook epub ./ ./wangyishengdegushi.epub
+# call gitbook pdf ./ ./王医生的故事.pdf
+# call gitbook mobi ./ ./王医生的故事.mobi
+call gitbook build
+# xcopy _book\*.* .\ /f /s /y
+# del _book\*.* /f /s /y
+# rd _book /s /Q
+@echo on
+call git add .
+call git commit -m "gitbook"
+call git config --global http.sslVerify "false"
+call git push origin gitbook
+pause
